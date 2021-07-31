62fcd3eecf7ba3996f83db669d29917e0efb43fd
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-07-31 22:27:19 +0530

    adding logger to seperate file

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const app = express()
-
+const logger = require('./logger')
 
 // req=> middleware => res
 // middileware should pass it next to middleware or back to the route , terminate by sending message,
-const logger = (req,res,next)=>{
-    const method = req.method;
-    const url = req.url;
-    const year = new Date().getFullYear();
-    console.log(method,url,year)
-    //res.send("logger")
-    next()
-}
+
 // express passes req,res to the middleware
 app.get('/',logger,(req,res)=>{
     res.send('Home')
